9d35327d227cd00dcba0952d39a0620d9428ac9f
ITByteEnthusiast <182819753+ITByteEnthusiast@users.noreply.github.com>
2025-06-24 02:43:56 -0400

    fix: pre-push again

--- scripts/githooks/pre-push.ps1
+++ scripts/githooks/pre-push.ps1
@@ -27,6 +27,11 @@ Write-Host "[DEBUG] scriptPath: $scriptPath"
 Write-Host "[DEBUG] scriptRoot: $scriptRoot"
 Write-Host "[DEBUG] repoRoot: $repoRoot"
 Write-Host "[DEBUG] sharedPath: $sharedPath"
+Write-Host "======"
+Write-Host "[DEBUG] repoRoot: $repoRoot"
+Write-Host "[DEBUG] sharedPath: $sharedPath"
+Write-Host "[DEBUG] LoggingUtils path: $(Join-Path $sharedPath 'LoggingUtils.psm1')"
+Write-Host "[DEBUG] TruffleHogShared path: $(Join-Path $sharedPath 'TruffleHogShared.psm1')"
 
 # Import shared modules
 Import-Module (Join-Path $sharedPath "LoggingUtils.psm1") -ErrorAction Stop
